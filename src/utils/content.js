4cfed1f63841ce01a013c7504ce57da3a9a07533
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-03-21 19:55:54 +0100

    correct content

--- src/utils/content.js
+++ src/utils/content.js
@@ -11,7 +11,7 @@ export default {
     home: {
       data: {
         desc:
-          "Rozpoczynasz nowy biznes, zakladasz organizacje? Pewnie potrzebujesz strony internetowej! W takim razie trafiłeś najlepiej jak mogłes.",
+          "Rozpoczynasz nowy biznes, zakładasz organizację? Pewnie potrzebujesz strony internetowej! W takim razie trafiłeś najlepiej jak mogłeś.",
       },
     },
     projects: {
@@ -27,7 +27,7 @@ export default {
           {
             head: "jestem finalistą konkursu cansat 2020",
             desc:
-              "Wraz ze znajomymi założyliśmy Team Trailblazer i jako drużyna wystartowaliśmy w konkursie organizowanym przez Europejską Agencję Kosmiczną, który polegał na zbudowaniu sondy. Ja w tym projekcie odpowiadałem za analizę zdjęć i danych, które zestawiłem na stronie WWW. Jesteśmy w szeregu 10 drużyn z całej Polski zawkwalifikowanych do finału i nasza sonda wykona swoją misję w czerwcu 2020",
+              "Wraz ze znajomymi założyliśmy Team Trailblazer i jako drużyna wystartowaliśmy w konkursie organizowanym przez Europejską Agencję Kosmiczną, który polegał na zbudowaniu sondy. Ja w tym projekcie odpowiadałem za analizę zdjęć i danych, które zestawiłem na stronie www. Jesteśmy w szeregu 10 drużyn z całej Polski zawkwalifikowanych do finału i nasza sonda wykona swoją misję w czerwcu 2020",
           },
           {
             head: "piszę artykuły na dev.to",
